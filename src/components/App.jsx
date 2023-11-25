ba1645f7aa4baed5b012e8a477584949788a01c4
vsreddyh <shouryanreddyh@gmail.com>
2023-11-25 12:32:10 +0530

    a

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 export default function App(){
     return(
         <BrowserRouter>
-        <SignIn />
             <Routes>
                 <Route path="/" element={<SignIn />} exact />
                 <Route path="/signup" element={<SignUp />} />
