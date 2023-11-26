94375db92fe3cd2a741b5e41258fe406f1397124
Nithin Chowdary <nithinchowdary2354@gmail.com>
2023-11-26 21:05:47 +0530

    emo

--- src/components/Check-Email.jsx
+++ src/components/Check-Email.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Check-Email.css"
+import Header from "./Header.jsx";
+import Sider from "./Sider.jsx";
+import "./signin.css"
+
 export  default function (){
     return (
         <div className="container">
-            <div className="cont">
-                <h1 className="heading">Check Your Email</h1>
-                <p className="para">We've sent you an email with a link to set your password. Please check your inbox and follow the instructions in the email.</p>
+             <Header />
+             <Sider />
+             <div className="content1" id="bodyy">
+                <div id="body-content">
+                    <p>
+                        check your mail 
+                    </p>
+                </div>
             </div>
         </div>
 
